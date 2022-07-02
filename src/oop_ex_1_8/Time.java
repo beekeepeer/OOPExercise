874253db7d8e7ad3fe7ba4560d58b26766e239bc
Dmitry Goryuk <dmitrygoryuk@gmail.com>
2022-07-02 10:51:17 +0300

    commit 1.8 again

---
+++ src/oop_ex_1_8/Time.java
@@ -0,0 +1,79 @@
+package oop_ex_1_8;
+
+public class Time {
+    private int hour, minute, second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toString() {
+        return "[" + String.format("%02d", hour) + ":" + String.format("%02d", minute) + ":"
+                + String.format("%02d", second) + "]";
+    }
+
+    public Time nextSecond() { // I did not know that it it's possible to write a method with a tipe of its
+                               // class!!!
+        second++;
+        if (second > 59) {
+            second = 0;
+            minute++;
+        }
+        if (minute > 59) {
+            minute = 0;
+            hour++;
+        }
+        if (hour > 23) {
+            hour = 0;
+        }
+        return this; // WOW!!! I dod not know that it is possible to retern "this"!
+    }
+
+    public Time previousSecond() {
+        second--;
+        if (second < 0) {
+            second = 59;
+            minute--;
+        }
+        if (minute < 0) {
+            minute = 59;
+            hour--;
+        }
+        if (hour < 0) {
+            hour = 23;
+        }
+        return this;
+    }
+}
